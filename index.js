e187985a07bc4744b864f33e2f923606b30b85cc
adarsh <lifoxctn@gmail.com>
2025-09-19 09:14:49 +0530

    pending

--- index.js
+++ index.js
@@ -1,19 +1,6 @@
 
 
-    function newcolor(selectedColor) {
-      document.body.style.backgroundColor = selectedColor;
+  const changecolor = document.getElementById("changecolor");
 
-      const backgroundClr = { color: selectedColor }; 
-      sessionStorage.setItem("backgroundClr", JSON.stringify(backgroundClr));
-    }
-
-    window.onload = () => {
-      const bgClr = sessionStorage.getItem("backgroundClr");
-      if (bgClr) {
-        const result = JSON.parse(bgClr);
-        document.body.style.backgroundColor = result.color;
-        document.getElementById("changecolor").value = result.color;
-      }
-    };
-
-
+    // Restore background from sessionStorage
+    const savedColor = sessionStorage.getItem("bgColor");
